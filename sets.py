f595f4678d90efedaa327ff377a31c1667930b2c
rachhoya <virinarachhoya@gmail.com>
2026-03-20 18:58:47 +0000

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,13 @@
+a={13,17,5,7,23,12}
+a.add(25)
+print(a)
+a.remove(7)
+print(a)
+
+x=set("virina")
+y=set("tree")
+print("union",x|y)
+print("intersection",x&y)
+print(x-y)
+print(y-x)
+print("symetric difference",x^y)
---
+++ tuple.py
@@ -0,0 +1,10 @@
+a=(1,2,3,[4,5,6])
+a[3].append(6)
+print(a)
+x=[5,32,17,13]
+y=tuple(x)
+print(y)
+
+w="hello"
+z=tuple(w)
+print(tuple(w))
